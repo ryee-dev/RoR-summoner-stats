f432f958afd17f831154ed6e098f6037c6991e50
roger yee <ryee.epicodus@gmail.com>
2019-03-31 16:07:34 -0700

    successfully fetches match history

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import useAxios from 'axios-hooks';
+import { SummonerForm } from './components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [{ data, loading, error }, refetch] = useAxios(
+    'http://localhost:3001/api/summoner'
+  );
+
+  const [summName, setSummName] = useState('');
+
+  const [fetchedData, setFetchedData] = useState('');
+  // @ts-ignore
+  const handleRefetch = e => {
+    e.preventDefault();
+    refetch();
+    setFetchedData(JSON.stringify(data, null, 2));
+  };
+
+  // @ts-ignore
+  const updateName = e => {
+    e.preventDefault();
+    setSummName(e.target.value);
+  };
+
+  return (
+    <AppShell>
+      <FloatingContainer>
+        <SummonerForm
+          summName={summName}
+          updateName={updateName}
+          handleRefetch={handleRefetch}
+        />
+        <br />
+        <div className="data-wrapper">
+          {loading && <p style={{ position: 'absolute' }}>loading</p>}
+          {error && <p>error</p>}
+          {data && <pre>{fetchedData}</pre>}
+        </div>
+
+        {/* <h1>{summName}</h1> */}
+      </FloatingContainer>
+    </AppShell>
+  );
+};
 
 export default App;
+
+const AppShell = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
+  padding: 2rem 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #06080b;
+  overflow: scroll;
+`;
+
+const FloatingContainer = styled.div`
+  background-color: #151a27;
+  border-radius: 4px;
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 4rem 0;
+  overflow: scroll;
+
+  h1 {
+    color: white;
+  }
+
+  .data-wrapper {
+    color: white;
+    margin: 2rem 0;
+    overflow: scroll;
+  }
+`;
--- index.js
+++ index.js
@@ -20,15 +20,20 @@ app.use((req, res, next) => {
   next();
 });
 
-// declare objects
 let summonerName = "contractz";
+
+// post summoner name input
+app.post('/api/summoner', async (req, res) => {
+  summonerName = req.body.summName;
+});
+
+// declare objects
 let summonerInfo;
-let matchHistory;
+let matchHistoryInfo;
 let result = [];
 
 // fetch data
 app.get('/api/summoner', async (req, res) => {
-
   let fetchedSummonerData = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.API_KEY}`);
 
   summonerInfo = {
@@ -36,12 +41,26 @@ app.get('/api/summoner', async (req, res) => {
     accountId: fetchedSummonerData.data.accountId,
   };
 
-  result.push(summonerInfo);
+  // const getMatches = async () => {
+  //   const summoner = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.API_KEY}`);
+  //   const { matches } = await axios.get(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summoner.data.accountId}?api_key=${process.env.API_KEY}`);
+  //   // const matchIds = matches.map(({ gameId }) => gameId);
+  //
+  //   // const results = await Promise.all(matchIds.map(matchId => `https://na1.api.riotgames.com/lol/match/v4/matches/${matchId}?api_key=${process.env.API_KEY}`));
+  //   return matches;
+  // };
 
+  let fetchedMatchHistory = await axios.get(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${summonerInfo.accountId}?api_key=${process.env.API_KEY}`);
+
+  matchHistoryInfo = {
+    matches: fetchedMatchHistory.data.matches,
+  };
 
   result = {
-    summonerData: summonerInfo
+    summonerData: summonerInfo,
+    matchHistoryData: matchHistoryInfo,
   };
+
 
   res.json(result);
 });
