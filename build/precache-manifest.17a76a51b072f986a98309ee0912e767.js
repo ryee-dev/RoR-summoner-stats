a7b04c43cda4de07fb1cc0e40fb6fb71a5c9732b
roger yee <ryee.epicodus@gmail.com>
2019-04-08 16:52:07 -0700

    build

---
+++ build/precache-manifest.17a76a51b072f986a98309ee0912e767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4be46bc5b7b9b70b899",
+    "url": "/static/css/main.6fc141f8.chunk.css"
+  },
+  {
+    "revision": "c4be46bc5b7b9b70b899",
+    "url": "/static/js/main.c4be46bc.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "76fd89bce71d0e09413e",
+    "url": "/static/js/2.76fd89bc.chunk.js"
+  },
+  {
+    "revision": "4f9288b8e4b76ca52b762d9c02277d4c",
+    "url": "/index.html"
+  }
+];
