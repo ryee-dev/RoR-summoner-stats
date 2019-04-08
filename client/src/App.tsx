ef805e94bd0638ee89bf7374149f7894b72d8421
roger yee <ryee.epicodus@gmail.com>
2019-04-08 01:13:04 -0700

    sets procfile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -140,52 +140,6 @@ const App = () => {
         </ModalWrapper>
       )}
 
-      {/* {modalStatus && !loading && data && ( */}
-      {/*  <ModalWrapper> */}
-      {/*    <ResultsModal> */}
-      {/*      <ListWrapper> */}
-      {/*        {data.hits.map((match: any) => ( */}
-      {/*          <MatchCard */}
-      {/*            key={match.gameId} */}
-      {/*            win={match.outcome} */}
-      {/*            gameDuration={match.gameDuration} */}
-      {/*            summonerName={match.summonerName} */}
-      {/*            summAId={match.spell1Id} */}
-      {/*            summBId={match.spell2Id} */}
-      {/*            keystone={match.runes.keystone} */}
-      {/*            primaryRune1={match.runes.primaryRune1} */}
-      {/*            primaryRune2={match.runes.primaryRune2} */}
-      {/*            primaryRune3={match.runes.primaryRune3} */}
-      {/*            secondaryRune1={match.runes.secondaryRune1} */}
-      {/*            secondaryRune2={match.runes.secondaryRune2} */}
-      {/*            championId={match.championId} */}
-      {/*            kills={match.kills} */}
-      {/*            deaths={match.deaths} */}
-      {/*            assists={match.assists} */}
-      {/*            kda={match.kda} */}
-      {/*            item0={match.items.item0} */}
-      {/*            item1={match.items.item1} */}
-      {/*            item2={match.items.item2} */}
-      {/*            item3={match.items.item3} */}
-      {/*            item4={match.items.item4} */}
-      {/*            item5={match.items.item5} */}
-      {/*            item6={match.items.item6} */}
-      {/*            champLevel={match.championLevel} */}
-      {/*            totalMinionsKilled={match.creepScore.totalMinionsKilled} */}
-      {/*            neutralMinionsKilled={match.creepScore.neutralMinionsKilled} */}
-      {/*            neutralMinionsKilledTeamJungle={ */}
-      {/*              match.creepScore.neutralMinionsKilledTeamJungle */}
-      {/*            } */}
-      {/*            neutralMinionsKilledEnemyJungle={ */}
-      {/*              match.creepScore.neutralMinionsKilledEnemyJungle */}
-      {/*            } */}
-      {/*          /> */}
-      {/*        ))} */}
-      {/*      </ListWrapper> */}
-      {/*    </ResultsModal> */}
-      {/*  </ModalWrapper> */}
-      {/* )} */}
-
       {!loading && modalStatus && error && (
         <div
           style={{
